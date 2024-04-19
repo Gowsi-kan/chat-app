5e2a40ed96434cd26fc1583d41b856b1291cd080
Gowsikan <gowsikan32412@gmail.com>
2024-04-19 16:07:17 +0530

    Front UI parts uploading

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+
+import React from 'react'
+import Conversation from './Conversation'
+
+const Conversations = () => {
+    return (
+        <div className='py-2 flex flex-col overflow-auto'>
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+        </div>
+    )
+}
+
+export default Conversations
---
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,13 @@
+import { BiLogOut } from "react-icons/bi";
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const LogoutButton = () => {
+    return (
+        <div className='mt-auto'>
+            <BiLogOut className='w-6 h-6 text-white cursor-pointer' />
+        </div>
+    )
+}
+
+export default LogoutButton
