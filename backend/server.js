65758aa1703b9fccf08c5322fcb015c4bce1ba71
Gowsikan <gowsikan32412@gmail.com>
2024-04-25 14:22:49 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/authroutes.js";
 import messageRoutes from "./routes/messageroutes.js";
@@ -11,19 +12,22 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
 
-// app.get("/", (req, res) => {
-//     // root route http://localhost:5000
-//     res.send("Hello World");
-// })
-
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 server.listen(PORT, () => {
     connectToMongoDB();
